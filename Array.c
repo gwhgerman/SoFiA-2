4d644d356b528e685dbf3dd0a576717e2c20f558
SoFiA-Admin <tobias.westmeier@uwa.edu.au>
2019-02-06 07:23:32 +0000

    Updated source code documentation... ...of file 'Array.c'.

--- Array.c
+++ Array.c
@@ -37,9 +37,26 @@
 
 
 
-// ------------------------------------------------- //
-// Standard constructor                              //
-// ------------------------------------------------- //
+// ----------------------------------------------------------------- //
+// Standard constructor                                              //
+// ----------------------------------------------------------------- //
+// Arguments:                                                        //
+//                                                                   //
+//   size - Size of the array to be created.                         //
+//                                                                   //
+// Return value:                                                     //
+//                                                                   //
+//   Pointer to newly created Array object.                          //
+//                                                                   //
+// Description:                                                      //
+//                                                                   //
+//   Standard constructor. Will create a new Array object of given   //
+//   size and type double and return a pointer to the newly created  //
+//   object. Sufficient memory will be allocated to store the array  //
+//   values of type double. Note that the destructor will need to be //
+//   called explicitly once the object is no longer required to re-  //
+//   lease any memory allocated during the lifetime of the object.   //
+// ----------------------------------------------------------------- //
 
 public Array *Array_new(const size_t size)
 {
@@ -63,9 +80,29 @@ public Array *Array_new(const size_t size)
 
 
 
-// ------------------------------------------------- //
-// Constructor from comma-separated values in string //
-// ------------------------------------------------- //
+// ----------------------------------------------------------------- //
+// Alternative constructor                                           //
+// ----------------------------------------------------------------- //
+// Arguments:                                                        //
+//                                                                   //
+//   string - String containing the values to be stored in the       //
+//            array, separated by commas.                            //
+//                                                                   //
+// Return value:                                                     //
+//                                                                   //
+//   Pointer to newly created Array object.                          //
+//                                                                   //
+// Description:                                                      //
+//                                                                   //
+//   Alternative constructor. Will create a new Array object of type //
+//   double, the size of which is determined by the number of comma- //
+//   separated floating-point values specified in 'string'. A point- //
+//   er to the newly created object will be returned. Sufficient me- //
+//   mory will be allocated to store the array values as type dou-   //
+//   ble. Note that the destructor will need to be called explicitly //
+//   once the object is no longer required to release any memory al- //
+//   located during the lifetime of the object.                      //
+// ----------------------------------------------------------------- //
 
 public Array *Array_new_str(char *string)
 {
@@ -97,9 +134,23 @@ public Array *Array_new_str(char *string)
 
 
 
-// ------------------------------------------------- //
-// Destructor                                        //
-// ------------------------------------------------- //
+// ----------------------------------------------------------------- //
+// Destructor                                                        //
+// ----------------------------------------------------------------- //
+// Arguments:                                                        //
+//                                                                   //
+//   (1) this     - Object self-reference.                           //
+//                                                                   //
+// Return value:                                                     //
+//                                                                   //
+//   No return value.                                                //
+//                                                                   //
+// Description:                                                      //
+//                                                                   //
+//   Destructor. Note that the destructor must be called explicitly  //
+//   if the object is no longer required. This will release the me-  //
+//   mory occupied by the object.                                    //
+// ----------------------------------------------------------------- //
 
 public void Array_delete(Array *this)
 {
